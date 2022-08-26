859fd0bc7f57de47c37a2e3e40e1cef2daa2f1f9
Lachlan <83265082+Lachlan2108@users.noreply.github.com>
2022-08-26 14:13:49 +1200

    awd
    awd

--- cise_ass1a_worksheet4/frontend/src/App.js
+++ cise_ass1a_worksheet4/frontend/src/App.js
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 import React from "react";
+=======
+import React, {components} from 'react';
+>>>>>>> Stashed changes
 import {
   Route,
   NavLink,
@@ -11,28 +15,36 @@ import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
 import NotFoundPage from "./pages/404";
 
-const App = () =>  {
+class App extends components {
+  render() {
     return (
-        <Router>
-        <div>
+      <Router>
+      <div>
           <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
             <ul className="header">
                 <li><NavLink exact to = "/">Home</NavLink></li>
-                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SE-Practice">Select the Practice</NavLink></li>
                 <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
           <div className="content">
+<<<<<<< Updated upstream
             <Route exact path="/" component={Home}/>
             <Route  path="/SEPractice" component={SEPractice}/>
             <Route  path="/SubmitArticle" component={SubmitArticle}/>
             <Route exact path="/404" component={NotFoundPage}/>
             <Redirect to="/404" />
+=======
+            <Route exact path="/" components={Home}/>
+            <Route  path="/SEPractice" components={SEPractice}/>
+            <Route  path="/SubmitArticle" components={SubmitArticle}/>
+            <Route exact path="/404" components={NotFoundPage}/>
+            <BrowserRouter to="/404" />
+>>>>>>> Stashed changes
             </div>
         </div>
         </Router>
-
     );
+  }
 }
- 
 
 export default App;
